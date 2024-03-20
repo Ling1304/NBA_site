4ca58fd6135cb2349c61c914be3e4dec2b0a9749
Ling1304 <117529856+Ling1304@users.noreply.github.com>
2024-03-20 12:49:55 +0800

    Update Home🏡.py

--- NBA_Prediction_Site/Home🏡.py
+++ NBA_Prediction_Site/Home🏡.py
@@ -4,7 +4,7 @@ st.set_page_config(
     page_title="Homepage"
 )
 
-st.image(image = "https://github.com/Ling1304/NBA_site/blob/main/NBA_Prediction_Site/NBA_finals.png")
+st.image(image = "https://github.com/Ling1304/NBA_site/tree/main/NBA_Prediction_Site/NBA_finals.png")
 st.title("Welcome to my Final Year Project!")
 st.write("I used Machine Learning to predict the 2023 NBA Championship Teams. The 2024 NBA Regular Season is about to end, so stay tuned for the prediction for the 2024 NBA Championship Teams!")
 
